a8f89f83140d6a8afcecb16c86832fd51f53d071
pr4j3sh <prajesh.eleven118@gmail.com>
2025-02-23 16:10:45 +0530

    fix: added check for prompt

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,8 @@ const { API_KEY } = require("./config");
 
 async function ai(prompt) {
   try {
+    if (!prompt) throw new Error("please provide a valid prompt");
+
     const res = await fetch(
       `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&key=${API_KEY}`,
     );
